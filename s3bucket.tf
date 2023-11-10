332c44b338f53f87f2646f38220dfdca0be1fa06
Adam Martin <adam.martin@hudl.com>
2023-11-10 15:22:14 +0000

    Reverting to simple s3 bucket

--- s3bucket.tf
+++ s3bucket.tf
@@ -1,10 +1,3 @@
 resource "aws_s3_bucket" "adam_test_bucket" {
   bucket   = "adam-test-bucket"
 }
-
-resource "aws_s3_bucket_ownership_controls" "adam_test_bucket" {
-  bucket   = "adam_test_bucket"
-  rule {
-    object_ownership = "BucketOwnerPreferred"
-  }
-}
