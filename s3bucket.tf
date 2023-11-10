52659ff4ce07002626e82f72599e79f642e52b88
Adam Martin <adam.martin@hudl.com>
2023-11-10 15:18:56 +0000

    Removing unsupported acl

--- s3bucket.tf
+++ s3bucket.tf
@@ -8,9 +8,3 @@ resource "aws_s3_bucket_ownership_controls" "adam_test_bucket" {
     object_ownership = "BucketOwnerPreferred"
   }
 }
-
-resource "aws_s3_bucket_acl" "adam_test_bucket" {
-  depends_on = [aws_s3_bucket_ownership_controls.adam_test_bucket]
-  bucket     = aws_s3_bucket.adam_test_bucket.id
-  acl        = "private"
-}
